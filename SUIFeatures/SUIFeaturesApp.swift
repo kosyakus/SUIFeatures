52f2dafb0471139fa806268b7f7c26e134f5b0fc
Natali <ns@numbuster.com>
2024-10-29 15:13:06 +0300

    Initial Commit

---
+++ SUIFeatures/SUIFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIFeaturesApp.swift
+//  SUIFeatures
+//
+//  Created by Natalia Sinitsyna on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUIFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
